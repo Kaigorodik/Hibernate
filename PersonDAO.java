ce135e2c8ded6a8dd4747fcd7dd7040c3069b35a
Kaigorodova Liubov <70796797+Kaigorodik@users.noreply.github.com>
2022-06-04 08:24:23 +0500

    Update PersonDAO.java

--- PersonDAO.java
+++ PersonDAO.java
@@ -1,34 +1,15 @@
-package ru.javastudy.hibernate.dao.interfaces;
+package ru.javastudy.hibernate.dao;
 
-import ru.javastudy.hibernate.dao.PersonEntity;
-import ru.javastudy.hibernate.dao.StudentEntity;
+import org.hibernate.Session;
+import ru.javastudy.hibernate.entities.Person;
 
-import java.util.List;
+public class PersonDAO extends AbstractDAO<Person> {
+    public PersonDAO(Session session) {
+        super(session);
+    }
 
-
-public interface PersonDAO {
-
-    // String hql = "FROM person";
-    // Query query = session.createQuery("FROM person WHERE INSTR (LOWER (firstName, lastName, middleName), 'а') > 0);
-    //List<PersonEntity> persons = query.list();
-
-    // String hql = "FROM student";
-    // Query query = session.createQuery("FROM student WHERE recordBook_id IS NULL);
-    //       List<StudentEntity> students = query.list();
-
-
-    // Найти все контакты.
-    public List<PersonEntity> findAll();
-
-    // Найти все контакты с заданным телефоном и хобби.
-    public List<PersonEntity> findAllWithDetail();
-
-    // Найти контакт со всеми деталями по идентификатору.
-    public PersonEntity findById(Long id);
-
-    // Вставить или обновить контакт.
-    public PersonEntity save(PersonEntity person);
-
-    // Удалить контакт.
-    public void deleteAll(StudentEntity student);
+    @Override
+    public String getEntityName() {
+        return "Person";
+    }
 }
